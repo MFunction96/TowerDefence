ea6281d966ab5bb2a1f59ff6b9c4d1a569e06e7a
MFunction <MFunction@cugb.edu.cn>
2017-04-15 10:13:00 +0800

    添加路径搜索类

---
+++ src/PathSearch/PathSearch.java
@@ -0,0 +1,11 @@
+package PathSearch;
+
+/**
+ * Created by MFunction on 2017/4/15.
+ * This Class is used to finding the path from start to end.
+ */
+public class PathSearch {
+    PathSearch() {
+        
+    }
+}

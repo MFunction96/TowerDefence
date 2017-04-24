24cd34fc4845006de881983a22b3d4cf92708c98
MFunction <MFunction@cugb.edu.cn>
2017-04-24 15:59:40 +0800

    优化组织结构

--- src/Controller/TimeController.java
+++ src/Controller/TimeController.java
@@ -2,6 +2,7 @@ package Controller;
 
 /**
  * Created by MFunction on 2017/4/17.
+ * 时间驱动类
  */
 public class TimeController {
 }
--- src/Monster/MonNormal.java
+++ src/Monster/MonNormal.java
@@ -2,6 +2,7 @@ package Monster;
 
 /**
  * Created by MFunction on 2017/4/19.
+ * 事件驱动类
  */
 public class MonNormal {
 }

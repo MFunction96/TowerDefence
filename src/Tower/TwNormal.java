9abe02b227c960265c1d580f88d06ec8ef6ebfe0
MFunction <MFunction96@users.noreply.github.com>
2017-05-20 20:29:41 +0800

    Chris young (#3)
    * 修改塔基础类的构造函数
    塔基础类添加设置塔的表现层和逻辑层位置的方法，初始化塔的表现侧和逻辑层位置属性
    塔基础类添加获得攻击目标的方法
    设定（静态）普通塔的各个属性
    
    * 修改塔基础类的构造函数
    塔基础类添加设置塔的表现层和逻辑层位置的方法，初始化塔的表现侧和逻辑层位置属性
    塔基础类添加获得攻击目标的方法
    设定（静态）普通塔的各个属性
    
    * 修改塔基础类的构造函数
    塔基础类添加设置塔的表现层和逻辑层位置的方法，初始化塔的表现侧和逻辑层位置属性
    塔基础类添加获得攻击目标的方法
    设定（静态）普通塔的各个属性
    
    Signed-off-by: ChrisYoung96 <yanghuaan96423@outlook.com>
    
    * 一、修改塔的基础类
    1、添加_canattack属性，用来标记塔是否可以攻击
    2、修改部分变量名称
    3、修改构造函数，方便后期生成地图时，对塔进行设置
    4、添加SetTower()方法，当塔被放置后，设置塔的表现层和业务层坐标，并设置塔为可攻击。
    5、修改Upgarde()方法错误
    二、修改Block类
    1、修改Block类部分变量名，更可读
    
    * 1、添加获取塔的状态的方法
    2、修改小区块AddTower方法

--- src/Tower/TwNormal.java
+++ src/Tower/TwNormal.java
@@ -1,10 +1,15 @@
 package Tower;
 
+import BaseClass.Tower;
+
 /**
  * Created by MFunction on 2017/4/19.
  */
-public class TwNormal {
-    public TwNormal(){
+public class TwNormal extends Tower {
 
+    public TwNormal(){
+        super("炮塔",1,2,6,1,1,3 );
     }
+
+
 }

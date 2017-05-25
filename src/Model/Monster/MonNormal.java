826ecde05193ccc5b80187a5f1b75a6a3b0cd208
ZhenYanXin <466390825@qq.com>
2017-05-25 13:49:48 +0800

    怪的界面图形

--- src/Model/Monster/MonNormal.java
+++ src/Model/Monster/MonNormal.java
@@ -2,7 +2,9 @@ package Model.Monster;
 
 import Model.BaseClass.Monster;
 import Model.BaseClass.Point;
+import View.GameMenu;
 
+import java.awt.*;
 import java.util.ArrayDeque;
 
 /**
@@ -11,8 +13,30 @@ import java.util.ArrayDeque;
  */
 public class MonNormal extends Monster{
 
+    private Image _nomal;
+    private GameMenu _menu;
+
+
     public MonNormal(Monster monster, ArrayDeque<Point> ad) {
         super(monster,ad);
+    }
+
+    /**
+     * 构造普通怪
+     * @param monster 基础怪的元对象
+     * @param ad 路径
+     * @param menu 怪所在视图
+     */
+    public MonNormal(Monster monster, ArrayDeque<Point> ad,GameMenu menu) {
+        super(monster,ad);
+        _menu=menu;
+        _nomal=Toolkit.getDefaultToolkit().getImage("src/Image/NormalMonster.png");
+    }
+    /*
+        普通怪的形态
+     */
+    public void draw(Graphics g) {
+        g.drawImage(_nomal,_surlocation.x(),_surlocation.y(),64,64,_menu);
     }
 
 }

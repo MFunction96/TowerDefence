14c2776360146621fead6b1bd16a357e0e3f5dad
ZhenYanXin <466390825@qq.com>
2017-05-28 13:50:33 +0800

    增加塔的安装按钮

--- src/View/URL/DrawTwNormal.java
+++ src/View/URL/DrawTwNormal.java
@@ -2,6 +2,7 @@ package View.URL;
 
 import javax.swing.*;
 import java.awt.*;
+import Model.BaseClass.Point;
 import  Model.* ;
 import Model.BaseClass.Tower;
 /**
@@ -13,12 +14,12 @@ import Model.BaseClass.Tower;
 
 public class DrawTwNormal extends JFrame  {
 
-    public void drawTw(String name,Tower tower ){
+    public void drawTw(String name,Tower tower,Point towerLocation ){
         if(name.equals("炮塔") ){
             JLabel _TwNormal ;
-
+            tower.SetTower(towerLocation,towerLocation);
             _TwNormal =new JLabel(new ImageIcon("src/Image/TwNormal.png") ) ;
-            _TwNormal .setBounds(900,300,64,64);
+            _TwNormal .setBounds(towerLocation.x(),towerLocation.y(),64,64);
             _TwNormal .setOpaque(false );//设置背景透明
             this.getContentPane() .add(_TwNormal );
         }
